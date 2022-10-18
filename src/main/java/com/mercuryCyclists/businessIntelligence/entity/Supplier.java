26b1bd1ae265522520f38a97c0a41b5e0ecfb48e
Pedro Oste <pedstarpedstar@yahoo.com.au>
2022-10-18 20:43:46 +1100

    Intial commit, cloned over from the procurement service

---
+++ src/main/java/com/mercuryCyclists/businessIntelligence/entity/Supplier.java
@@ -0,0 +1,41 @@
+package com.mercuryCyclists.businessIntelligence.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Supplier Entity / DAO
+ */
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Supplier {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String companyName;
+
+    private String base;
+
+    @OneToMany(cascade=CascadeType.ALL, orphanRemoval=true)
+    private Set<Contact> contactSet = new HashSet<Contact>();
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Set<Long> backOrders = new HashSet<>();
+
+    /**
+     * Checks to see whether supplier is valid
+     */
+    public boolean validate(){
+        if (companyName == null || companyName.isEmpty() || base == null || base.isEmpty() ){
+            return false;
+        }
+        return true;
+    }
+}
